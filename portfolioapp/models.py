4c5771ba9e8c337fd1e4a0b1e1c62d31cad61eb0
Courdevelops <courdevelops@gmail.com>
2022-01-28 16:02:29 -0500

    models done

--- portfolioapp/models.py
+++ portfolioapp/models.py
@@ -75,3 +75,97 @@ class Testimonial(models.Model):
 
     def __str__(self):
         return self.name
+
+#MEDIA
+class Media(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Media Files'
+        verbose_name = 'Media'
+        ordering = ["name"]
+    
+    image = models.ImageField(blank=True, null=True, upload_to="media")
+    url = models.URLField(blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    is_image = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.url:
+            self.is_image = False 
+        super(Media, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.name 
+
+
+#PORTFOLIO
+class Portfolio(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Portfolio Profiles'
+        verbose_name = 'Portfolio'
+        ordering = ["name"]
+    
+    date = models.DateTimeField(blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True, upload_to="portfolio")
+    slug = models.SlugField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(Portfolio, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name 
+    
+    def get_absolute_url(self):
+        return f"/portfolio/{self.slug}"
+
+    
+    #BLOG
+    class Blog(models.Model):
+
+        class Meta:
+            verbose_name_plural = 'Blog Profiles'
+            verbose_name = 'Blog'
+            ordering = ["timestamp"]
+
+        timestamp = models.DateTimeField(auto_now_add=True)
+        author = models.CharField(max_length=200, blank=True, null=True)
+        name = models.CharField(max_length=200, blank=True, null=True)
+        description = models.CharField(max_length=500, blank=True, null=True)
+        body = RichTextField(blank=True, null=True)
+        slug = models.SlugField(null=True, blank=True)
+        image = models.ImageField(blank=True, null=True, upload_to="blog")
+        is_active = models.BooleanField(default=True)
+
+        def save(self, *args, **kwargs):
+            if not self.id:
+                self.slug = slugify(self.name)
+            super(Blog, self).save(*args, **kwargs) 
+
+        def __str__(self):
+            return self.name 
+        
+        def get_absolute_url(self):
+            return f"/blog/{self.slug}"
+
+
+#CERTIFICATE
+class Certificate(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Certificates'
+        verbose_name = 'Certificate'
+
+    date = models.DateTimeField(blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name 
